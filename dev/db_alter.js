a04338df77e8e984818036061689826d9b51c92f
noodlesaddle <tserenbal@miraic.com>
2023-10-31 16:14:42 +0900

    initManager creds updated

--- dev/db_alter.js
+++ dev/db_alter.js
@@ -7,12 +7,12 @@ const bcrypt = require("bcryptjs");
 const models_1 = require("../backend/models");
 const console_1 = require("console");
 const moment = require("moment");
-const isDBAlter = process.argv.find(argc => argc == '-dbAlter');
-const isDBForce = process.argv.find(argc => argc == '-dbForce');
-const isDBSeed = process.argv.find(argc => argc == '-dbSeed');
+const isDBAlter = process.argv.find((argc) => argc == "-dbAlter");
+const isDBForce = process.argv.find((argc) => argc == "-dbForce");
+const isDBSeed = process.argv.find((argc) => argc == "-dbSeed");
 let dbInitConfig = {
     alter: false,
-    force: false
+    force: false,
 };
 if (isDBForce) {
     dbInitConfig.force = true;
@@ -24,45 +24,52 @@ models_1.db.sequelize
     .sync(dbInitConfig)
     .then(async () => {
     return seedDB(models_1.db);
-}).then(() => {
-    (0, console_1.log)('db finished');
+})
+    .then(() => {
+    (0, console_1.log)("db finished");
     process.exit(0);
-}).catch((e) => {
+})
+    .catch((e) => {
     console.warn(e);
-    fs.appendFile('../logs/log_rest.log', `db init error ${e.toString()}, ${JSON.stringify(e)}. ${(new Date()).toISOString()} \n`, console.warn);
+    fs.appendFile("../logs/log_rest.log", `db init error ${e.toString()}, ${JSON.stringify(e)}. ${new Date().toISOString()} \n`, console.warn);
     process.exit(1);
 });
 async function seedDB(db) {
+    if (!(process.env.INIT_MANAGER_USERNAME &&
+        process.env.INIT_MANAGER_PW &&
+        process.env.INIT_MANAGER_EMAIL)) {
+        throw new Error("initialManager credential is not set");
+    }
     if (isDBSeed) {
-        if (await db.models.managers.findOne() == null) {
+        if ((await db.models.managers.findOne()) == null) {
             await db.models.managers.create({
-                username: 'pcr_admin',
-                pwhash: await bcrypt.hash('8368z8h8ad', 10),
-                recoveryMail: 'support@line-sys.com',
-                isActivated: true
+                username: process.env.INIT_MANAGER_USERNAME,
+                pwhash: await bcrypt.hash(process.env.INIT_MANAGER_PW, 10),
+                recoveryMail: process.env.INIT_MANAGER_EMAIL,
+                isActivated: true,
             });
         }
         await db.models.customers.create({
-            firstName: '名前',
-            lastName: '苗字',
-            firstNameKana: 'なまえ',
-            lastNameKana: 'みょうじ',
-            email: 'test@line-sys.com',
-            telephone: '01234567890',
-            zipPostal: '4600003',
-            prefecture: '愛知県',
-            city: '名古屋市',
-            address: '錦2-12-34',
-            pwhash: await bcrypt.hash('test', 10)
+            firstName: "名前",
+            lastName: "苗字",
+            firstNameKana: "なまえ",
+            lastNameKana: "みょうじ",
+            email: "test@line-sys.com",
+            telephone: "01234567890",
+            zipPostal: "4600003",
+            prefecture: "愛知県",
+            city: "名古屋市",
+            address: "錦2-12-34",
+            pwhash: await bcrypt.hash("test", 10),
         });
         let occasionPCR = await db.models.occasions.create({
-            type: 'pcr',
-            title: 'PCR TEST',
-            telephone: '12365478900',
-            address: '名古屋駅',
-            zipPostal: '4600003',
+            type: "pcr",
+            title: "PCR TEST",
+            telephone: "12365478900",
+            address: "名古屋駅",
+            zipPostal: "4600003",
             timeCancel: 1,
-            textCancel: 'text Cancel test',
+            textCancel: "text Cancel test",
             canCancel: false,
             cancelMessage: `[NAME]様\n[DATE]にご予約キャンセルされました。\nご利用ありがとうございます。改めてご予約の場合は下記のリンクをを押してください。\n　https://${process.env.SITE_URI}`,
             regMessage: `[NAME]様\nご利用ありがとうございます。\nご予約完了になりました。\n検査の当日[DATE]QRコードをスタッフに見せてください。\n　https://${process.env.SITE_URI}`,
@@ -71,35 +78,37 @@ async function seedDB(db) {
             limitTime: true,
             limitDays: 1,
             limitHours: 18,
-            limitMinutes: 0
+            limitMinutes: 0,
         });
         let fakeOccurrences = [];
-        let startDateTime = moment('2022-02-28 09:00:00');
+        let startDateTime = moment("2023-    09:00:00");
         for (let i = 0; i < 20; i++) {
             startDateTime.hour(9).minute(0);
             for (let j = 0; j < 8; j++) {
                 fakeOccurrences.push({
                     occasionId: occasionPCR.occasionId,
                     maxAttendee: Math.floor(Math.random() * (30 - 20) + 20),
-                    startAt: moment(startDateTime).format('YYYY-MM-DD HH:mm:ss'),
-                    endAt: moment(startDateTime).add(59, 'minutes').format('YYYY-MM-DD HH:mm:ss'),
-                    startTime: startDateTime.format('HH:mm:ss')
+                    startAt: moment(startDateTime).format("YYYY-MM-DD HH:mm:ss"),
+                    endAt: moment(startDateTime)
+                        .add(59, "minutes")
+                        .format("YYYY-MM-DD HH:mm:ss"),
+                    startTime: startDateTime.format("HH:mm:ss"),
                 });
-                startDateTime.add(1, 'hour');
+                startDateTime.add(1, "hour");
             }
-            startDateTime.add(1, 'day');
+            startDateTime.add(1, "day");
         }
         await db.models.occurrences.bulkCreate(fakeOccurrences, {
-            fields: ['occasionId', 'maxAttendee', 'startAt', 'endAt', 'startTime']
+            fields: ["occasionId", "maxAttendee", "startAt", "endAt", "startTime"],
         });
         let occasionAntigen = await db.models.occasions.create({
-            type: 'antigen',
-            title: 'ANTIGEN TEST',
-            telephone: '12365478900',
-            address: '名古屋駅',
-            zipPostal: '4600003',
+            type: "antigen",
+            title: "ANTIGEN TEST",
+            telephone: "12365478900",
+            address: "名古屋駅",
+            zipPostal: "4600003",
             timeCancel: 1,
-            textCancel: 'text Cancel test',
+            textCancel: "text Cancel test",
             canCancel: false,
             cancelMessage: `[NAME]様\n[DATE]にご予約キャンセルされました。\nご利用ありがとうございます。改めてご予約の場合は下記のリンクをを押してください。\n　https://${process.env.SITE_URI}`,
             regMessage: `[NAME]様\nご利用ありがとうございます。\n予約完了になりました。\n検査の当日[DATE]QRコードをスタッフに見せてください。\n　https://${process.env.SITE_URI}`,
@@ -108,48 +117,50 @@ async function seedDB(db) {
             limitTime: true,
             limitDays: 1,
             limitHours: 18,
-            limitMinutes: 0
+            limitMinutes: 0,
         });
         let fakeOccurrences1 = [];
-        let startDateTime1 = moment('2022-02-28 09:00:00');
+        let startDateTime1 = moment("2023-11-10 09:00:00");
         for (let i = 0; i < 20; i++) {
             startDateTime1.hour(9).minute(0);
             for (let j = 0; j < 8; j++) {
                 fakeOccurrences1.push({
                     occasionId: occasionAntigen.occasionId,
                     maxAttendee: Math.floor(Math.random() * (30 - 20) + 20),
-                    startAt: moment(startDateTime1).format('YYYY-MM-DD HH:mm:ss'),
-                    endAt: moment(startDateTime1).add(59, 'minutes').format('YYYY-MM-DD HH:mm:ss'),
-                    startTime: startDateTime1.format('HH:mm:ss')
+                    startAt: moment(startDateTime1).format("YYYY-MM-DD HH:mm:ss"),
+                    endAt: moment(startDateTime1)
+                        .add(59, "minutes")
+                        .format("YYYY-MM-DD HH:mm:ss"),
+                    startTime: startDateTime1.format("HH:mm:ss"),
                 });
-                startDateTime1.add(1, 'hour');
+                startDateTime1.add(1, "hour");
             }
-            startDateTime1.add(1, 'day');
+            startDateTime1.add(1, "day");
         }
         await db.models.occurrences.bulkCreate(fakeOccurrences1, {
-            fields: ['occasionId', 'maxAttendee', 'startAt', 'endAt', 'startTime']
+            fields: ["occasionId", "maxAttendee", "startAt", "endAt", "startTime"],
         });
     }
 }
 exports.seedDB = seedDB;
-process.on('uncaughtException', function (err) {
+process.on("uncaughtException", function (err) {
     if (err) {
         if (process.env.ENV_TEST) {
             console.warn(err);
         }
         else {
-            fs.appendFile('../logs/log_rest.log', `uncaught exception. stack: ${err.stack} ${(new Date()).toISOString()} \n`, console.warn);
+            fs.appendFile("../logs/log_rest.log", `uncaught exception. stack: ${err.stack} ${new Date().toISOString()} \n`, console.warn);
         }
         process.exit(1);
     }
 });
-process.on('exit', code => {
+process.on("exit", (code) => {
     if (code) {
         if (process.env.ENV_TEST) {
             console.warn(code);
         }
         else {
-            fs.appendFile('../logs/log_rest.log', `uncaught exception. stack: ${code} ${(new Date()).toISOString()} \n`, console.warn);
+            fs.appendFile("../logs/log_rest.log", `uncaught exception. stack: ${code} ${new Date().toISOString()} \n`, console.warn);
         }
         process.exit(1);
     }
